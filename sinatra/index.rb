ecde4f4c3ac313ae3cb54e67b3815704a399a80c
Robert Bolinger <rbolinger86@gmail.com>
2018-11-01 12:45:05 -0500

    Info not submitting.

--- sinatra/index.rb
+++ sinatra/index.rb
@@ -12,31 +12,44 @@ end
 def get_birth_number (birth_number)
   case birth_number
   when 1
-    puts "Your numerology number is #{birth_number}.\nOne is the leader. The number one indicates the ability to stand alone and is a strong vibration. Ruled by the Sun."
+    message = "Your numerology number is #{birth_number}.\nOne is the leader. The number one indicates the ability to stand alone and is a strong vibration. Ruled by the Sun."
   when 2
-    puts "Your numerology number is #{birth_number}.\nThis is the mediator and peace-lover. The number two indicates the desire for harmony. It is a gentle, considerate, and sensitive vibration. Ruled by the Moon."
+    message = "Your numerology number is #{birth_number}.\nThis is the mediator and peace-lover. The number two indicates the desire for harmony. It is a gentle, considerate, and sensitive vibration. Ruled by the Moon."
   when 3
-    puts "Your numerology number is #{birth_number}.\nNumber Three is a sociable, friendly, and outgoing vibration. Kind, positive, and optimistic, Three's enjoy life and have a good sense of humor. Ruled by Jupiter."
+    message = "Your numerology number is #{birth_number}.\nNumber Three is a sociable, friendly, and outgoing vibration. Kind, positive, and optimistic, Three's enjoy life and have a good sense of humor. Ruled by Jupiter."
   when 4
-    puts "Your numerology number is #{birth_number}.\nThis is the worker. Practical, with a love of detail, Fours are trustworthy, hard-working, and helpful. Ruled by Uranus."
+    message = "Your numerology number is #{birth_number}.\nThis is the worker. Practical, with a love of detail, Fours are trustworthy, hard-working, and helpful. Ruled by Uranus."
   when 5
-    puts "Your numerology number is #{birth_number}.\nThis is the freedom lover. The number five is an intellectual vibration. These are 'idea' people with a love of variety and the ability to adapt to most situations. Ruled by Mercury."
+    message = "Your numerology number is #{birth_number}.\nThis is the freedom lover. The number five is an intellectual vibration. These are 'idea' people with a love of variety and the ability to adapt to most situations. Ruled by Mercury."
   when 6
-    puts "Your numerology number is #{birth_number}.\nThis is the peace lover. The number six is a loving, stable, and harmonious vibration. Ruled by Venus."
+    message = "Your numerology number is #{birth_number}.\nThis is the peace lover. The number six is a loving, stable, and harmonious vibration. Ruled by Venus."
   when 7
-    puts "Your numerology number is #{birth_number}.\nThis is the deep thinker. The number seven is a spiritual vibration. These people are not very attached to material things, are introspective, and generally quiet. Ruled by Neptune."
+    message = "Your numerology number is #{birth_number}.\nThis is the deep thinker. The number seven is a spiritual vibration. These people are not very attached to material things, are introspective, and generally quiet. Ruled by Neptune."
   when 8
-    puts "Your numerology number is #{birth_number}.\nThis is the manager. Number Eight is a strong, successful, and material vibration. Ruled by Saturn."
+    message = "Your numerology number is #{birth_number}.\nThis is the manager. Number Eight is a strong, successful, and material vibration. Ruled by Saturn."
   when 9
-    puts "Your numerology number is #{birth_number}.\nThis is the teacher. Number Nine is a tolerant, somewhat impractical, and sympathetic vibration. Ruled by Mars."
+    message = "Your numerology number is #{birth_number}.\nThis is the teacher. Number Nine is a tolerant, somewhat impractical, and sympathetic vibration. Ruled by Mars."
   else
-    puts "Uh oh! Your birth path number is not 1-9!"
+    message = "Uh oh! Your birth path number is not 1-9!"
   end
 end
 
 get '/:birthdate' do
  birthdate = params[:birthdate]
  birth_number = birthpath(birthdate)
- message = get_birth_number(birth_number)
- "#{message}"
+ @message = get_birth_number(birth_number)
+ erb :index
+end
+
+get '/newpage' do
+  @message = "Hey, what are you doin' here!?"
+  erb :newpage
+end
+
+get '/' do
+  erb :form
+end
+
+post '/' do
+  "#{params}"
 end
